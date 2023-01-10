f8c1475fb1b5787aba1d62de403c86d13e897cc6
Yashraj Deshmukh <107096154+yashrajsd@users.noreply.github.com>
2023-01-10 12:32:57 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,7 @@ import {getStorage} from 'firebase/storage'
 import {getAuth} from 'firebase/auth'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDm2Qklubrw78jNG_jr8ckd5aE7GBtsrgc",
-    authDomain: "changeorg-24a12.firebaseapp.com",
-    projectId: "changeorg-24a12",
-    storageBucket: "changeorg-24a12.appspot.com",
-    messagingSenderId: "1061835888261",
-    appId: "1:1061835888261:web:f996a5ea4c473349d9bbec"
+   config deleted
 };
 
 const app = initializeApp(firebaseConfig);
